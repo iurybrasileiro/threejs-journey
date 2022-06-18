3074b71937fcefd3e01467110f4a5c3992566ecc
Iury Brasileiro <Iury.brasileiro@outlook.com.br>
2022-06-18 15:02:39 -0300

    chapter 1

---
+++ 14-go-live/src/script.js
@@ -0,0 +1,158 @@
+import './style.css'
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader.js'
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry.js'
+import * as dat from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const textMatcapTexture = textureLoader.load('textures/matcaps/7.png')
+const donutMatcapTexture = textureLoader.load('textures/matcaps/6.png')
+
+/**
+ * Fonts
+ */
+const fontLoader = new FontLoader()
+
+fontLoader.load(
+    '/fonts/helvetiker_regular.typeface.json',
+    (font) =>
+    {
+        // Material
+        const textMaterial = new THREE.MeshMatcapMaterial({ matcap: textMatcapTexture })
+        const donutMaterial = new THREE.MeshMatcapMaterial({ matcap: donutMatcapTexture })
+        const cubeMaterial = new THREE.MeshNormalMaterial()
+
+        // Text
+        const textGeometry = new TextGeometry(
+            'Iury Brasileiro',
+            {
+                font: font,
+                size: 0.5,
+                height: 0.2,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 0.03,
+                bevelSize: 0.02,
+                bevelOffset: 0,
+                bevelSegments: 5
+            }
+        )
+        textGeometry.center()
+
+        const text = new THREE.Mesh(textGeometry, textMaterial)
+        scene.add(text)
+
+        // Donuts
+        const donutGeometry = new THREE.TorusGeometry(0.3, 0.2, 32, 64)
+
+        // Cube
+        const cubeGeometry = new THREE.BoxGeometry(1, 1, 1)
+
+        for(let i = 0; i < 1000; i++)
+        {
+            const donut = new THREE.Mesh(donutGeometry, donutMaterial)
+            donut.position.x = (Math.random() - 0.5) * 40
+            donut.position.y = (Math.random() - 0.5) * 40
+            donut.position.z = (Math.random() - 0.5) * 40
+            donut.rotation.x = Math.random() * Math.PI
+            donut.rotation.y = Math.random() * Math.PI
+            const scale = Math.random()
+            donut.scale.set(scale, scale, scale)
+
+            scene.add(donut)
+
+            const cube = new THREE.Mesh(cubeGeometry, cubeMaterial)
+            cube.position.x = (Math.random() - 0.5) * 40
+            cube.position.y = (Math.random() - 0.5) * 40
+            cube.position.z = (Math.random() - 0.5) * 40
+            cube.rotation.x = Math.random() * Math.PI
+            cube.rotation.y = Math.random() * Math.PI
+            cube.scale.set(scale, scale, scale)
+
+            scene.add(cube)
+        }
+    }
+)
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+const clock = new THREE.Clock()
+
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
+
+    // Update controls
+    controls.update()
+
+    // Render
+    renderer.render(scene, camera)
+
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
